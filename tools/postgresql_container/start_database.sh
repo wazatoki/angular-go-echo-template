c95673c22eaa5ea9526eaccb2e21b16b95c7c64b
wazatoki <wazatoki@gmail.com>
2019-11-16 06:59:56 +0900

    db config setup

--- tools/postgresql_container/start_database.sh
+++ tools/postgresql_container/start_database.sh
@@ -2,8 +2,8 @@
 
 docker run --name postgresql\
 	-v /home/vagrant/tools/postgresql_container/postgresql:/mnt\
-	-e POSTGRES_USER=demo\
-	-e POSTGRES_PASSWORD=demo\
-	-e POSTGRES_DB=demodb\
+	-e POSTGRES_USER=sample\
+	-e POSTGRES_PASSWORD=sample\
+	-e POSTGRES_DB=sampledb\
 	-p 5432:5432\
 	-d postgres:11.3
